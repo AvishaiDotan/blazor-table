95535a0c6dd58eee6e499599856950c664ce9a23
Avishai Dotan <108017307+AvishaiDotan@users.noreply.github.com>
2023-02-12 22:23:53 +0200

    trying to implement classes

---
+++ BlazorContentTable/Shared/TableBodyClass.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorContentTable.Shared
+{
+    public class TableBodyClass
+    {
+        //public int id { get; set; }
+        //public string Username { get; set; }
+        //public bool Signed { get; set; }
+        //public object Created { get; set; }
+        //public int Age { get; set; }
+        public Dictionary<string, object> Data { get; set; }
+
+    }
+}
--- BlazorContentTable/Shared/TableData.cs
+++ BlazorContentTable/Shared/TableData.cs
@@ -5,6 +5,6 @@ namespace BlazorContentTable.Shared
     public class TableData
     {
         public List<TableHeaderClass> tableHeader { get; set; }
-        public List<TableContent> tableContent { get; set; }
+        public List<TableBodyClass> tableBody { get; set; }
     }
 }
